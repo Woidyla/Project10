b4be283ba75535289e12d422a0c61eb06924f3ed
Woidyla <142675593+Woidyla@users.noreply.github.com>
2023-10-24 20:51:05 -0400

    Update scripts.js
    Fix for Dark/Light mode

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Dark/Light mode
 const body = document.querySelector('body');
 const modeToggle = document.getElementById('mode-toggle');
 const modeStatus = document.querySelector('.modeStat');
@@ -12,6 +13,9 @@ function toggleMode() {
   modeStatus.innerText = modeMessage;
 }
 
+modeToggle.addEventListener('click', toggleMode);
+
+//Image slider
 let imgArray = [
   'https://raw.githubusercontent.com/Woidyla/mywebsite/main/Media/eliot.jpg',
     'https://raw.githubusercontent.com/Woidyla/Project10/main/8205FBB3-97A7-4675-BB26-AD5DAECFF2FE.jpeg',
@@ -33,3 +37,102 @@ function slideShow() {
     setTimeout(slideShow, imgDuration);
 }
 slideShow();
+
+//contact validation
+
+const form = document.getElementById('theForm')
+const submitButton = document.querySelector('.submit')
+const successMessage = document.getElementById('form-submitted-msg')
+
+const formElements = [ ...form.elements ]
+
+let allInputsValid = () => {
+  let valid = formElements.every((element) => {
+    if (element.nodeName === 'SELECT') {
+      return element.value !== 'Please select an option'
+    } else {
+      return element.checkValidity()
+    }
+  })
+
+
+  return valid
+}
+
+let handleChange = () => {
+  formElements.forEach((element) => {
+    if (!element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'  
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+      element.nextElementSibling.style.display = 'block'
+      element.previousElementSibling.style.color = 'red'
+    }
+    if (element.checkValidity()
+          && element.nodeName !== 'BUTTON'
+          && element.nodeName !== 'SELECT'
+          && element.type !== 'checkbox'
+          && element.type !== 'radio'
+    ) {
+      element.style.borderColor = 'green'
+      element.nextElementSibling.style.color = 'green'
+      element.nextElementSibling.style.display = 'none'
+      element.previousElementSibling.style.color = 'green'
+    }
+    if (!element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+    }
+    if (element.checkValidity()
+          && (element.type === 'checkbox'
+              || element.type === 'radio')
+    ) {
+      element.style.borderColor = 'green'
+      element.nextElementSibling.style.color = 'green'
+    }
+    if (element.nodeName === 'SELECT'
+          && element.value === 'Please select an option'
+    ) {
+      element.style.borderColor = 'red'
+      element.nextElementSibling.style.color = 'red'
+      element.nextElementSibling.style.display = 'block'
+      element.previousElementSibling.style.color = 'red'
+    }
+    if (element.nodeName === 'SELECT'
+          && element.value !== 'Please select an option'
+    ) {
+      element.style.borderColor = 'green'
+      element.nextElementSibling.style.color = 'green'
+      element.nextElementSibling.style.display = 'none'
+      element.previousElementSibling.style.color = 'green'
+    }
+  })
+  if (allInputsValid()) {
+    submitButton.removeAttribute('disabled', '')
+  } else {
+    submitButton.setAttribute('disabled', '')
+  }
+}
+
+let handleSubmit = (e) => {
+  e.preventDefault()
+  if (allInputsValid()) {
+    successMessage.style.display = 'block'
+    form.reset()
+    submitButton.setAttribute('disabled', '')
+    setTimeout(() => {
+      successMessage.style.display = 'none'
+    }, 3000)
+  }
+}
+formElements.forEach((element) => {
+  element.addEventListener('change', handleChange)
+})
+form.addEventListener('submit', (e) => handleSubmit(e))
